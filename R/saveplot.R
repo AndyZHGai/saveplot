c10d138170f3e3bed3d1c2909ae27ed4b75a22f1
ubuntu <ubuntu@localhost.localdomain>
2021-09-15 15:17:50 +0800

    version 0.0.1

---
+++ R/saveplot.R
@@ -0,0 +1,39 @@
+#' Save a plot as PDF
+#'
+#' @param filename  Filename to save to an PDF
+#' @param plot A plot object
+#' @param width Width of the image in inches
+#' @param height Height of the image in inches
+#' @param ... other parameters
+#'
+#' @return
+#' @export
+#'
+#' @author Zhonghui Gai
+#' @examples
+#' saveplot(filename = "test.pdf", plot(1:10))
+saveplot <- function(plot = plot(1:10), filename = "test", width = 10, height = 10, ...) {
+  stopifnot(!missing(plot))
+  if (missing(filename)) {
+    filename <- paste0(sample(LETTERS, 1), sample(letters, 1), sample(0:9, 1))
+  } else {
+    filename <- paste0(filename, sample(0:9, 1), ".pdf")
+  }
+  if (inherits(plot, "ggplot")) {
+    ggplot2::ggsave(filename = filename,
+                          plot = plot,
+                          width = width,
+                          height = height)
+  }
+  if (inherits(plot, "pheatmap")) {
+    pdf(file = filename, width = width, height = height)
+    grid::grid.newpage()
+    grid::grid.draw(plot$gtable)
+  }
+  if (inherits(plot, "NULL")) {
+    pdf(paste0(filename, ".pdf"), width = width, height = height)
+    plot
+  }
+  grDevices::dev.off()
+}
+
